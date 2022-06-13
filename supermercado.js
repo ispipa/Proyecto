9b5a24ad4a3f59c19f4909112a7cfa07ffff1ec2
Nylander26 <wolfnylander@gmail.com>
2022-06-13 13:37:35 +0200

    estilos, supers y supers.js

--- supermercado.js
+++ supermercado.js
@@ -2,32 +2,40 @@
 var buscar = document.getElementById('buscar');
 var busqueda = document.getElementById('busqueda');
 var ul = document.getElementById('superLista');
-
 var li = document.createElement('li');
-var btn = document.createElement('button');
+
 
 //Funciones
 busqueda.addEventListener("click", function(){
   
-  ul.appendChild(li);
-  li.innerHTML = buscar.value;
+  var texto = buscar.value;
+  if(texto){
+    
+    var li = document.createElement('li');
+    var btn = document.createElement('button');
+      btn.id = "btnEliminar";
+    var textoLi = document.createTextNode(texto);
+    var textoBtn = document.createTextNode("Eliminar");
+    btn.appendChild(textoBtn);
+    li.appendChild(textoLi);
+    li.appendChild(btn);
+    ul.appendChild(li);
+    btn.innerHTML = "Eliminar";
+  } else {
+    alert(console.log("No hay nada"));
+  }
+
+
+  function eliminar(){
+    ul.removeChild(li);
+  }
+
+  function eliminarAccindental(){
   
-  ul.appendChild(btn);
-  btn.innerHTML = "Eliminar";
-});
-
-superLista.addEventListener("click", eliminarAccindental, eliminar); 
-
-function eliminar(){
-
-  ul.removeChild(li);
-  ul.removeChild(btn);
-}
-
-function eliminarAccindental(){
-
-  if (addEventListener) "click", eliminar 
+    if (addEventListener) "click", eliminar 
     if (confirm("Desea eliminar el supermercado?")) {
       eliminar();
     }
   }
+  btn.addEventListener("click", eliminarAccindental);
+});
