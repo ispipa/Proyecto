ebb909641a233a62338458070fc25d02c33b7bbb
Nylander26 <wolfnylander@gmail.com>
2022-06-13 09:33:26 +0200

    varios mods

--- supermercado.js
+++ supermercado.js
@@ -1,14 +1,33 @@
+//Variables
 var buscar = document.getElementById('buscar');
 var busqueda = document.getElementById('busqueda');
+var ul = document.getElementById('superLista');
 
-busqueda.addEventListener("click", supermercado);
+var li = document.createElement('li');
+var btn = document.createElement('button');
 
-function supermercado(){
-  var ul = document.getElementById('superLista');
-  var li = document.createElement('li');
-  var btn = document.createElement('button');
-
-  superLista.appendChild(li);
-  superLista.appendChild(btn);
+//Funciones
+busqueda.addEventListener("click", function(){
+  
+  ul.appendChild(li);
   li.innerHTML = buscar.value;
+  
+  ul.appendChild(btn);
+  btn.innerHTML = "Eliminar";
+});
+
+superLista.addEventListener("click", eliminarAccindental, eliminar); 
+
+function eliminar(){
+
+  ul.removeChild(li);
+  ul.removeChild(btn);
 }
+
+function eliminarAccindental(){
+
+  if (addEventListener) "click", eliminar 
+    if (confirm("Desea eliminar el supermercado?")) {
+      eliminar();
+    }
+  }
